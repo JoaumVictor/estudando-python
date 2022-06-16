794a6cb4455ed60d2e3bd229c4f5ebaa36c96e70
Victor <joaumvictor.oficial@gmail.com>
2022-06-16 15:07:29 -0300

    pokemon game

--- atividades/exercicio4/pokemon_game2.py
+++ atividades/exercicio4/pokemon_game2.py
@@ -8,26 +8,42 @@ with open("pokemons.json") as file:
         poke_first_generation.append(pokemon["name"])
         if i == 151:
             break
-    # [pokemon["name"] for pokemon in pokemons[:151]]
+
+# with open("pokemons.json") as file:
+# poke_first_generation = [pokemon["name"] for pokemon in pokemons[:151]]
 
 points = 0
 finish = False
+round = 1
 
 
 def generate_poke_random():
     return random.choice(poke_first_generation)
 
+
+def message_round():
+    global round
+    if round == 5:
+        print("Uma Fera! 🔥")
+    if round == 10:
+        print("Uma Máquina! 🔥")
+    if round == 15:
+        print("Uma Lenda! 🔥")
+
+
 def run_game():
-    global points, finish
+    global points, finish, round
     count = 1
     result = ""
     broken = False
     poke_random = generate_poke_random()
     print("===================================")
+    print(f"Rodada: {round} / Pontuação: {points}")
+    message_round()
     while count != len(poke_random) - 1:
-        print(poke_random[:count])
+        print(poke_random[:count], "_" * (len(poke_random) - len(poke_random[:count])))
         count += 1
-        result = input("Quem é esse pokemon?")
+        result = input("Quem é esse pokemon? 🧐\n")
         if str.lower(result) == str.lower(poke_random):
             print("Parabéns!")
             broken = False
@@ -41,19 +57,22 @@ def run_game():
         print("Acabaram suas chances!")
         print(f"O pokemon era o {poke_random}!")
     print("Fim de round!")
+    round += 1
     print("===================================")
 
 run_game()
+
 
 while True:
     if finish:
         print(f"Você fez {points} pontos!")
         break
     print(f"Você tem {points} pontos!")
-    answer_continue = input("Deseja continuar? (y/n)")
-    if str.lower(answer_continue) == "y":
+    answer_continue = input("Deseja continuar? (Y/n)")
+    if str.lower(answer_continue) == "y" or str.lower(answer_continue) == '':
         run_game()
     else:
         break
+
 
 print("Fim de jogo!")
