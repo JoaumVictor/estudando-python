21073498b9d7db46cdaff18f31357ca73ace6f05
JoaumVictor <joaumvictor.oficial@gmail.com>
2023-02-26 15:09:50 -0300

    feat: implement salaryCalculator in conteudos folder

---
+++ conteudos/salaryCalculator.py
@@ -0,0 +1,9 @@
+from pySimpleGUI import PySimpleGUI as sg
+
+sg.theme('DarkAmber')   # Add a touch of color
+# All the stuff inside your window.
+layout = [  [sg.Text('Some text on Row 1')],
+            [sg.Text('Enter something on Row 2'), sg.InputText()],
+            [sg.Button('Ok'), sg.Button('Cancel')] ]
+
+sg.Window('Window Title', layout).read()
